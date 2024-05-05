c8f2aee2da2ad2d5fbd02982c138918a0f301993
StevenSimEEP <145448953+StevenSimEEP@users.noreply.github.com>
2024-05-05 17:05:55 +0200

    Clase UniversidadJsonTest creada y test de prueba realizado

---
+++ Spring/universidad/src/test/java/com/example/universidad/UniversidadJsonTest.java
@@ -0,0 +1,13 @@
+package com.example.universidad;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class UniversidadJsonTest {
+
+	@Test
+	void miPrimerTest() {
+		assertThat(42).isEqualTo(42);
+	}
+}
