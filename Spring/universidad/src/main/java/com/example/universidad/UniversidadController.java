b0937e2bc596300fffb1f77990629a2315997d26
StevenSimEEP <145448953+StevenSimEEP@users.noreply.github.com>
2024-05-07 10:50:42 +0200

    Cambios en el pomp por h2 en vez de mysql, de momento

--- Spring/universidad/src/main/java/com/example/universidad/UniversidadController.java
+++ Spring/universidad/src/main/java/com/example/universidad/UniversidadController.java
@@ -3,11 +3,13 @@ package com.example.universidad;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@CrossOrigin
 @RestController
 @RequestMapping("/universidades")
 public class UniversidadController {
