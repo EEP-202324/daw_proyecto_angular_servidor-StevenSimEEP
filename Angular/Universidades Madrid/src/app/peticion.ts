5474eaa375e594469c01462e364642cae24a5fbb
StevenSimEEP <145448953+StevenSimEEP@users.noreply.github.com>
2024-05-09 20:09:57 +0200

    Borrando código sobrante en Angular

--- Angular/Universidades Madrid/src/app/peticion.ts
+++
@@ -1,7 +0,0 @@
-import { subscriptionLogsToBeFn } from "rxjs/internal/testing/TestScheduler";
-
-export interface Peticion {
-  nombre?: string | null;
-  apellido?: string | null;
-  email?: string | null;
-}
--- Angular/Universidades Madrid/src/app/universidad.service.ts
+++ Angular/Universidades Madrid/src/app/universidad.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Universidad } from './universidad';
 import { HttpClient } from '@angular/common/http';
-import { Peticion } from './peticion';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversidadService {
-  url = 'http://localhost:3000/locations';
-  urlPeticion = 'http://localhost:8080/peticiones';
+  url = 'http://localhost:8080/universidades';
 
   private universidadActualizadaSubject = new Subject<Universidad>();
 
@@ -25,19 +23,6 @@ export class UniversidadService {
 
   getUniversidadById(id: number) {
     return this.http.get<Universidad>(`${this.url}/${id}`);
-  }
-
-  submitApplication(nombre: string, apellido: string, email: string) {
-    alert(`Petición de: ${nombre} ${apellido} (${email})`)
-  }
-
-  enviarPeticion(peticion: Peticion): Observable<Peticion> {
-    alert("petición recibida");
-    return this.http.post<Peticion>(this.url, peticion);
-  }
-
-  borrarPeticion(): Observable<Peticion> {
-    return this.http.delete(`${this.url}/c008`);
   }
 
   agregarUniversidad(universidadDatos: Omit<Universidad, 'id'>): Observable<Universidad> {
